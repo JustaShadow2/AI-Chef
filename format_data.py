334c0a74e13c187f9d2750c4c5ecfff6146eba93
[Arjun] <[aatwal27@uwo.ca]>
2024-11-10 04:19:55 -0500

    nvm now its formatted right

--- format_data.py
+++ format_data.py
@@ -4,17 +4,11 @@ import pandas as pd
 data = pd.read_csv('Time-to-spoil.csv')
 
 # Find all entries where the food is Fish, Beef, Chicken, Poultry, or Pork
-food_data = data[data['Food'].isin(['Milk', 'Cheese', 'Egg'])]
+food_data = data[data['Food'].isin(['Meat'])]
 
-# Get a list of the time to spoilage for the meat category
-time_to_spoilage = food_data[['Time to spoilage (days)', 'Storage Method']].values.tolist()
+# Find all entries where the storage method is raw
+storage_data = food_data[food_data['Storage Method'].isin(['Raw'])]
 
-print(time_to_spoilage)
-
-#make a new entry in formatted-data.csv as Meat, time_to_spoilage
-new_entry = {'Category': 'Dairy', 'Time to spoilage': time_to_spoilage}
-formatted_data = pd.read_csv('formatted-data.csv')
-formatted_data = formatted_data._append(new_entry, ignore_index=True)
-formatted_data.to_csv('formatted-data.csv', index=False)
+print(storage_data)
 
 
